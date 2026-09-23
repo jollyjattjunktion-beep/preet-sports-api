818918939485f0f20e92edbb3995114c6dd99960
jollyjattjunktion-beep <jollyjattjunktion@gmail.com>
2026-09-23 14:43:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(cors({
         "https://www.preetsports.cu.ma"
     ]
 }));
-
+app.use((req, res, next) => {
+    console.log(`REQUEST: ${req.method} ${req.url}`);
+    next();
+});
 let browser = null;
 let page = null;
 let lastData = null;
